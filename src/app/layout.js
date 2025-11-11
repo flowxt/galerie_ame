fce10dc24e2578c8844d102c016316d22bbe95be
florian <florianbarjon@icloud.com>
2025-11-11 18:18:26 +0100

    favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -90,6 +90,22 @@ export const metadata = {
   verification: {
     google: "nCI6GIs7AQSq6l7WRzNvFmODJ8VIqMEc3jHMVCeAcDo",
   },
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+    other: [
+      {
+        rel: "manifest",
+        url: "/site.webmanifest",
+      },
+    ],
+  },
   alternates: {
     canonical: "https://atelierartame.com",
   },
